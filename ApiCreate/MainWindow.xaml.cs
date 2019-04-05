f9a409bc7abd4e27976dcba926d365539079aa4d
Kabirf70 <kabirf70@gmail.com>
2019-04-06 00:17:59 +0600

    Api commit

---
+++ ApiCreate/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApiCreate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RestClient restClient = new RestClient();
+
+            restClient.endPoint = TextBox1.Text;
+            
+            string strResponse = string.Empty;
+
+            strResponse = restClient.makeRequest();
+            //TextBox2.Text = strResponse;
+            JArray response = JArray.Parse(strResponse);
+            List<Post> posts = Post.FromJsonArray(response);
+            listview2.ItemsSource=posts;
+
+    }       
+    }
+}
---
+++ ApiCreate/Post.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiCreate
+{
+    public class Post
+    {
+        private string title;
+        private string body;
+
+        public string Title { get => title; set => title = value; }
+        public string Body { get => body; set => body = value; }
+
+        public Post(JObject jObject)
+        {
+            this.Title = (string)jObject.GetValue("title");
+            this.Body = (string)jObject.GetValue("body");
+        }
+
+        public static List<Post> FromJsonArray(JArray postArray)
+        {
+            List<Post> posts = new List<Post>();
+            foreach (JObject jobject in postArray)
+            {
+                posts.Add(new Post(jobject));
+
+            }
+
+            return posts;
+        } 
+    }
+}
